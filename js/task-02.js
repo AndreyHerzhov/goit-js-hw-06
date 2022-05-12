4e841ae5d191d19aa4705f67f1f75aecf8beab13
AndreyHerzhov <andreyherzhov@gmail.com>
2022-05-12 12:03:14 +0300

    task10

--- js/task-02.js
+++ js/task-02.js
@@ -13,9 +13,9 @@ const ingredientsList = document.querySelector('#ingredients')
 const newList = ingredients.map( item => {
   const ingedientItem = document.createElement("li");
   ingedientItem.classList.add('item')
+  ingedientItem.textContent = item; //  textContent  
+  // ingedientItem.insertAdjacentHTML("beforeend", item); // insertAdjacentHTML
   // ingedientItem.innerHTML = item; // innerHTML
-  // ingedientItem.textContent = item; //  textContent  
-  ingedientItem.insertAdjacentHTML("beforeend", item); // insertAdjacentHTML
   return ingedientItem;
 })
 
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,43 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  input: document.querySelector('input'),
+  createBtn: document.querySelector('button[data-create]'),
+  destroyBtn: document.querySelector('button[data-destroy]'),
+  boxes: document.querySelector('#boxes'),
+  div: document.querySelector('.div-inside')
+}
+console.log(refs.input)
+console.log(refs.createBtn)
+console.log(refs.destroyBtn)
+
+refs.createBtn.addEventListener('click',createBoxes )
+
+let styleSize = 30;
+ 
+
+function createBoxes (amount) {
+  amount = refs.input.value;
+  console.log(amount)
+  for (let i = 0; i < amount; i += 1) {
+    const newDiv = document.createElement('div')
+    newDiv.classList.add('div-inside')
+    refs.boxes.append(newDiv)
+    newDiv.style.backgroundColor =  getRandomHexColor()
+    newDiv.style.width = `${styleSize}px`;
+    newDiv.style.height = `${styleSize}px`;
+    console.log(styleSize)
+    styleSize += 10;
+    
+  }
+}
+refs.destroyBtn.addEventListener('click', destroyBoxes)
+
+function destroyBoxes() {
+  refs.boxes.innerHTML = ''
+  refs.input.value = 0;
+}
+
+
